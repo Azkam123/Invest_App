a727f1c0a44413e0a0e5d00b27ad9ddd2176a5a7
moh hasan <mohhasan9998@gmail.com>
2025-06-23 03:22:14 +0700

    menambahkan analytics

---
+++ lib/pages/analytics_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class AnalyticsPage extends StatelessWidget {
+  const AnalyticsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Analisis Pasar'),
+        automaticallyImplyLeading: false, // Sembunyikan tombol kembali
+      ),
+      body: const Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.bar_chart, size: 80, color: Colors.blueAccent),
+              SizedBox(height: 20),
+              Text(
+                'Lihat Grafik & Analisis Pasar di Sini!',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Fitur grafik interaktif dan data historis akan segera hadir.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
